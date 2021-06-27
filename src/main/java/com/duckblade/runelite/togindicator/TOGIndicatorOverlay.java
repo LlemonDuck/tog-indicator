07fa63c73ac41c773aa6bfac8be0d4dcd64bd35f
LlemonDuck <napkinorton@gmail.com>
2021-06-27 13:32:13 -0400

    Auto-hide on skill tab hover (v1.1)

--- src/main/java/com/duckblade/runelite/togindicator/TOGIndicatorOverlay.java
+++ src/main/java/com/duckblade/runelite/togindicator/TOGIndicatorOverlay.java
@@ -1,7 +1,16 @@
 package com.duckblade.runelite.togindicator;
 
+import com.google.common.collect.BiMap;
+import com.google.common.collect.HashBiMap;
+import java.awt.Dimension;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+import java.awt.image.BufferedImage;
+import javax.inject.Inject;
+import javax.inject.Singleton;
 import net.runelite.api.Client;
 import net.runelite.api.Skill;
+import net.runelite.api.widgets.JavaScriptCallback;
 import net.runelite.api.widgets.Widget;
 import net.runelite.api.widgets.WidgetInfo;
 import net.runelite.client.ui.overlay.Overlay;
@@ -9,99 +18,125 @@ import net.runelite.client.ui.overlay.OverlayLayer;
 import net.runelite.client.ui.overlay.OverlayPosition;
 import net.runelite.client.util.ImageUtil;
 
-import javax.inject.Inject;
-import javax.inject.Singleton;
-import java.awt.*;
-import java.awt.image.BufferedImage;
-import java.util.HashMap;
-import java.util.Map;
-
 @Singleton
-public class TOGIndicatorOverlay extends Overlay {
+public class TOGIndicatorOverlay extends Overlay
+{
 
-    private static final Map<Skill, Integer> widgetIds = new HashMap<>();
+	private static final BiMap<Skill, Integer> widgetIds = HashBiMap.create(24);
 
-    static {
-        widgetIds.put(Skill.ATTACK, 20971521);
-        widgetIds.put(Skill.STRENGTH, 20971522);
-        widgetIds.put(Skill.DEFENCE, 20971523);
-        widgetIds.put(Skill.RANGED, 20971524);
-        widgetIds.put(Skill.PRAYER, 20971525);
-        widgetIds.put(Skill.MAGIC, 20971526);
-        widgetIds.put(Skill.RUNECRAFT, 20971527);
-        widgetIds.put(Skill.CONSTRUCTION, 20971528);
-        widgetIds.put(Skill.HITPOINTS, 20971529);
-        widgetIds.put(Skill.AGILITY, 20971530);
-        widgetIds.put(Skill.HERBLORE, 20971531);
-        widgetIds.put(Skill.THIEVING, 20971532);
-        widgetIds.put(Skill.CRAFTING, 20971533);
-        widgetIds.put(Skill.FLETCHING, 20971534);
-        widgetIds.put(Skill.SLAYER, 20971535);
-        widgetIds.put(Skill.HUNTER, 20971536);
-        widgetIds.put(Skill.MINING, 20971537);
-        widgetIds.put(Skill.SMITHING, 20971538);
-        widgetIds.put(Skill.FISHING, 20971539);
-        widgetIds.put(Skill.COOKING, 20971540);
-        widgetIds.put(Skill.FIREMAKING, 20971541);
-        widgetIds.put(Skill.WOODCUTTING, 20971542);
-        widgetIds.put(Skill.FARMING, 20971543);
-        widgetIds.put(Skill.OVERALL, 20971544);
-    }
+	static
+	{
+		widgetIds.put(Skill.ATTACK, 20971521);
+		widgetIds.put(Skill.STRENGTH, 20971522);
+		widgetIds.put(Skill.DEFENCE, 20971523);
+		widgetIds.put(Skill.RANGED, 20971524);
+		widgetIds.put(Skill.PRAYER, 20971525);
+		widgetIds.put(Skill.MAGIC, 20971526);
+		widgetIds.put(Skill.RUNECRAFT, 20971527);
+		widgetIds.put(Skill.CONSTRUCTION, 20971528);
+		widgetIds.put(Skill.HITPOINTS, 20971529);
+		widgetIds.put(Skill.AGILITY, 20971530);
+		widgetIds.put(Skill.HERBLORE, 20971531);
+		widgetIds.put(Skill.THIEVING, 20971532);
+		widgetIds.put(Skill.CRAFTING, 20971533);
+		widgetIds.put(Skill.FLETCHING, 20971534);
+		widgetIds.put(Skill.SLAYER, 20971535);
+		widgetIds.put(Skill.HUNTER, 20971536);
+		widgetIds.put(Skill.MINING, 20971537);
+		widgetIds.put(Skill.SMITHING, 20971538);
+		widgetIds.put(Skill.FISHING, 20971539);
+		widgetIds.put(Skill.COOKING, 20971540);
+		widgetIds.put(Skill.FIREMAKING, 20971541);
+		widgetIds.put(Skill.WOODCUTTING, 20971542);
+		widgetIds.put(Skill.FARMING, 20971543);
+		widgetIds.put(Skill.OVERALL, 20971544);
+	}
 
-    private final Client client;
+	private final Client client;
 
-    private final BufferedImage questIcon;
+	private final BufferedImage questIcon;
 
-    @Inject
-    public TOGIndicatorOverlay(Client client) {
-        this.client = client;
-        this.questIcon = ImageUtil.loadImageResource(TOGIndicatorOverlay.class, "/quest-icon.png");
-        setPosition(OverlayPosition.DYNAMIC);
-        setLayer(OverlayLayer.ABOVE_WIDGETS);
-    }
+	private Skill hoveredSkill;
 
-    @Override
-    public Dimension render(Graphics2D graphics) {
-        Widget skillsContainer = client.getWidget(WidgetInfo.SKILLS_CONTAINER);
-        if (skillsContainer == null || skillsContainer.isHidden())
-            return null;
+	@Inject
+	public TOGIndicatorOverlay(Client client)
+	{
+		this.client = client;
+		this.questIcon = ImageUtil.loadImageResource(TOGIndicatorOverlay.class, "/quest-icon.png");
+		setPosition(OverlayPosition.DYNAMIC);
+		setLayer(OverlayLayer.ABOVE_WIDGETS);
+	}
 
-        int minExp = Integer.MAX_VALUE;
-        for (Skill s : Skill.values()) {
-            int skillExp = client.getSkillExperience(s);
-            if (skillExp < minExp) {
-                minExp = skillExp;
-            }
-        }
+	@Override
+	public Dimension render(Graphics2D graphics)
+	{
+		Widget skillsContainer = client.getWidget(WidgetInfo.SKILLS_CONTAINER);
+		if (skillsContainer == null || skillsContainer.isHidden())
+			return null;
 
-        for (Skill s : Skill.values()) {
-            // check for min xp
-            int skillExp = client.getSkillExperience(s);
-            if (skillExp != minExp)
-                continue;
+		int minExp = Integer.MAX_VALUE;
+		for (Skill s : Skill.values())
+		{
+			int skillExp = client.getSkillExperience(s);
+			if (skillExp < minExp)
+			{
+				minExp = skillExp;
+			}
+		}
 
-            // get widget id
-            int widgetId = widgetIds.getOrDefault(s, -1);
-            if (widgetId == -1)
-                continue;
+		for (Skill s : Skill.values())
+		{
+			if (hoveredSkill != null && s != hoveredSkill)
+				continue;
 
-            // find widget from id
-            Widget skillWidget = null;
-            for (Widget w : skillsContainer.getStaticChildren()) {
-                if (w.getId() == widgetId) {
-                    skillWidget = w;
-                    break;
-                }
-            }
-            if (skillWidget == null)
-                continue;
+			// check for min xp
+			int skillExp = client.getSkillExperience(s);
+			if (skillExp != minExp)
+				continue;
 
-            // draw icon over skill box
-            Rectangle r = skillWidget.getBounds();
-            graphics.drawImage(questIcon, r.x + r.width - questIcon.getWidth(), r.y, null);
-        }
+			// get widget id
+			int widgetId = widgetIds.getOrDefault(s, -1);
+			if (widgetId == -1)
+				continue;
 
-        return null;
-    }
+			// find widget from id
+			Widget skillWidget = null;
+			for (Widget w : skillsContainer.getStaticChildren())
+			{
+				if (w.getId() == widgetId)
+				{
+					skillWidget = w;
+					break;
+				}
+			}
+			if (skillWidget == null)
+				continue;
+
+			// draw icon over skill box
+			Rectangle r = skillWidget.getBounds();
+			graphics.drawImage(questIcon, r.x + r.width - questIcon.getWidth(), r.y, null);
+		}
+
+		return null;
+	}
+
+	public void attachHoverListeners()
+	{
+		Widget skillsContainer = client.getWidget(WidgetInfo.SKILLS_CONTAINER);
+		if (skillsContainer == null)
+		{
+			return;
+		}
+
+		for (Widget skillWidget : skillsContainer.getStaticChildren())
+		{
+			final Skill skill = widgetIds.inverse().get(skillWidget.getId());
+			if (skill != null)
+			{
+				skillWidget.setOnMouseOverListener((JavaScriptCallback) event -> hoveredSkill = skill);
+			}
+		}
+		skillsContainer.setOnMouseLeaveListener((JavaScriptCallback) event -> hoveredSkill = null);
+	}
 
 }
--- src/main/java/com/duckblade/runelite/togindicator/TOGIndicatorPlugin.java
+++ src/main/java/com/duckblade/runelite/togindicator/TOGIndicatorPlugin.java
@@ -1,30 +1,54 @@
 package com.duckblade.runelite.togindicator;
 
+import javax.inject.Inject;
+import net.runelite.api.Client;
+import net.runelite.api.GameState;
+import net.runelite.api.events.WidgetLoaded;
+import net.runelite.api.widgets.WidgetInfo;
+import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
 import net.runelite.client.ui.overlay.OverlayManager;
 
-import javax.inject.Inject;
-
 @PluginDescriptor(
-        name = "ToG Indicator"
+		name = "ToG Indicator"
 )
-public class TOGIndicatorPlugin extends Plugin {
+public class TOGIndicatorPlugin extends Plugin
+{
 
-    @Inject
-    private OverlayManager overlayManager;
+	@Inject
+	private OverlayManager overlayManager;
 
-    @Inject
-    private TOGIndicatorOverlay overlay;
+	@Inject
+	private TOGIndicatorOverlay overlay;
 
-    @Override
-    protected void startUp() throws Exception {
-        overlayManager.add(overlay);
-    }
+	@Inject
+	private Client client;
 
-    @Override
-    protected void shutDown() throws Exception {
-        overlayManager.remove(overlay);
-    }
+	@Override
+	protected void startUp() throws Exception
+	{
+		overlayManager.add(overlay);
+
+		if (client.getGameState() == GameState.LOGGED_IN)
+		{
+			overlay.attachHoverListeners();
+		}
+	}
+
+	@Override
+	protected void shutDown() throws Exception
+	{
+		overlayManager.remove(overlay);
+	}
+
+	@Subscribe
+	public void onWidgetLoaded(WidgetLoaded widget)
+	{
+		if (widget.getGroupId() == WidgetInfo.SKILLS_CONTAINER.getGroupId())
+		{
+			overlay.attachHoverListeners();
+		}
+	}
 
 }
